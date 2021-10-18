428c2d1daf6d4a6fc79a5a64e4c5486dbc455916
gemaquejr <gemaquejr@hotmail.com>
2021-10-18 19:53:14 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const speciesOfAnimalsById = [];
+  ids.forEach((id) => {
+    speciesOfAnimalsById.push(species.find((specie) => specie.id === id));
+  });
+  return speciesOfAnimalsById;
 }
 
 module.exports = getSpeciesByIds;
