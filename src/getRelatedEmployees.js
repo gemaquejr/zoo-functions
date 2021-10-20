bdb39b0b32e7fb5360cd4689f9e3c17009f54a65
gemaquejr <gemaquejr@hotmail.com>
2021-10-19 23:47:29 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const managerIndex = employees.some((employee) => employee.managers.includes(id));
+  return managerIndex;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const collaborators = employees.filter((employee) => employee.managers.includes(managerId));
+    return collaborators.map((names) => `${names.firstName} ${names.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
