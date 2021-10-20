73815e91d2f2a29ca2ea2f0b20b467191be3b334
gemaquejr <gemaquejr@hotmail.com>
2021-10-19 21:00:23 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const employeeFirstName = employees.find((employee) => employee.firstName === employeeName);
+  const employeeLastName = employees.find((employee) => employee.lastName === employeeName);
+  if (employeeFirstName) {
+    return employeeFirstName;
+  }
+  if (employeeLastName) {
+    return employeeLastName;
+  }
 }
 
 module.exports = getEmployeeByName;
