83cd5929f34ce166c0c8fcf4e7d073a5a0067936
gemaquejr <gemaquejr@hotmail.com>
2021-10-20 10:28:20 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeId = employees.find((employee) => employee.id === id);
+  const oldestEmployee = employeeId.responsibleFor[0];
+  const speciesAndEmployee = species.find((specie) => specie.id === oldestEmployee);
+  const oldestAnimal = speciesAndEmployee.residents.sort((a, b) => b.age - a.age);
+  return Object.values(oldestAnimal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
