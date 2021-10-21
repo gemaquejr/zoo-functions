ea31d03edf963ec5b78d1438c9e669b0612505c1
gemaquejr <gemaquejr@hotmail.com>
2021-10-21 13:55:50 -0300

    Adiciona requisito 7 para conseguir 80 graças a Deus

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+function speciesSex(specie, options) {
+  if ('sex' in options) {
+    return specie.residents
+      .filter((resident) => resident.sex === options.sex)
+      .map((resident) => resident.name);
+  }
+  return specie.residents.map((resident) => resident.name);
+}
+
+function namesOfSpecies(options) {
+  if ('includeNames' in options) {
+    const coordinates = { NE: [], NW: [], SE: [], SW: [] };
+    species.forEach((specie) => {
+      const { name, location } = specie;
+      const animalNames = speciesSex(specie, options);
+      if ('sorted' in options) animalNames.sort();
+      coordinates[location].push({ [name]: animalNames });
+    });
+    return coordinates;
+  }
+}
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (options === undefined || options.includeNames === undefined) {
+    const coordinates = { NE: [], NW: [], SE: [], SW: [] };
+    species.forEach((specie) => {
+      const { name, location } = specie;
+      coordinates[location].push(name);
+    });
+    return coordinates;
+  }
+  return namesOfSpecies(options);
 }
 
 module.exports = getAnimalMap;
