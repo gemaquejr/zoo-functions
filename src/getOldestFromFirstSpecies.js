5a545ffb5ffc084922c3ca4cd4d1b3c438f56422
gemaquejr <gemaquejr@hotmail.com>
2023-03-08 17:18:32 -0300

    Adiciona refatoracao da funcao getOldestFromFirstSpecies.js

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,12 +1,13 @@
 const data = require('../data/zoo_data');
-const { employees, species } = require('../data/zoo_data');
+
+const { employees, species } = data;
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
   const employeeId = employees.find((employee) => employee.id === id);
-  const oldestEmployee = employeeId.responsibleFor[0];
-  const speciesAndEmployee = species.find((specie) => specie.id === oldestEmployee);
-  const oldestAnimal = speciesAndEmployee.residents.sort((a, b) => b.age - a.age);
+  const specieId = employeeId.responsibleFor[0];
+  const currentSpecie = species.find((specie) => specie.id === specieId);
+  const oldestAnimal = currentSpecie.residents.sort((a, b) => b.age - a.age);
   return Object.values(oldestAnimal[0]);
 }
 
