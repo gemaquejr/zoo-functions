98834469563555832b87a4a3d832e121bf757661
gemaquejr <gemaquejr@hotmail.com>
2023-03-08 17:28:25 -0300

    Adiciona refatoracao da funcao getRelatedEmployees.js

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,5 +1,6 @@
 const data = require('../data/zoo_data');
-const { employees } = require('../data/zoo_data');
+
+const { employees } = data;
 
 function isManager(id) {
   // seu código aqui
@@ -9,11 +10,11 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
-  if (isManager(managerId)) {
-    const collaborators = employees.filter((employee) => employee.managers.includes(managerId));
-    return collaborators.map((names) => `${names.firstName} ${names.lastName}`);
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  const collaborators = employees.filter((employee) => employee.managers.includes(managerId));
+  return collaborators.map((names) => `${names.firstName} ${names.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
