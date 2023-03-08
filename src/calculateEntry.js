1a1993a5b7a64d018865d30b52835183e5f95179
gemaquejr <gemaquejr@hotmail.com>
2023-03-08 09:05:16 -0300

    Adiciona refatoracao da funcao calculateEntry.js

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,30 +1,24 @@
 const data = require('../data/zoo_data');
-const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
-  if (entrants === undefined || Object.entries(entrants).length < 1) {
-    return 0;
-  }
-  const child = entrants.filter((entrant) => entrant.age < 18).length;
-  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
-  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
-  return { child, adult, senior };
+  const child = entrants.filter((personAge) => personAge.age < 18).length;
+  const adult = entrants.filter((personAge) => personAge.age >= 18 && personAge.age < 50).length;
+  const senior = entrants.filter((personAge) => personAge.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
-function calculateEntry(entries) {
+function calculateEntry(entrants) {
   // seu código aqui
-  const entrants = countEntrants(entries);
-  if (entrants === undefined) {
-    return 0;
-  }
-  const totalChild = entrants.child * prices.child;
-  const totalAdult = entrants.adult * prices.adult;
-  const totalSenior = entrants.senior * prices.senior;
-  const totalPeoples = totalChild + totalAdult + totalSenior;
-  if (!totalPeoples) {
-    return 0;
-  }
+  if (!entrants || Object.keys(entrants).length < 1) return 0;
+  const totalPerson = countEntrants(entrants);
+  const { child, adult, senior } = totalPerson;
+  const { prices } = data;
+  const totalPeoples = ((child * prices.child) + (adult * prices.adult) + (senior * prices.senior));
   return totalPeoples;
 }
 
