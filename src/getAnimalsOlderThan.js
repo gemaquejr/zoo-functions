aeec0ee36ec38970765428b48c7650f39307796f
gemaquejr <gemaquejr@hotmail.com>
2023-03-08 15:25:43 -0300

    Adiciona refatoracao da funcao getAnimalsOlderThan.js

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
-const { species } = require('../data/zoo_data');
+
+const { species } = data;
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const speciesOfAnimals = species.find((specie) => specie.name === animal);
-  const speciesOfAnimalsAge = speciesOfAnimals.residents.every((animalAge) => animalAge.age >= age);
-  return speciesOfAnimalsAge;
+  const speciesOfAnimals = species.find((specie) => specie.name === animal).residents;
+  return speciesOfAnimals.every((animalAge) => animalAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
